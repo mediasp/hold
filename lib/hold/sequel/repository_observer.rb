900150770b53dee33029bdb2bc19a2cdb6082bc2
Tom Chipchase <tom@mediasp.com>
2015-02-16 15:54:24 +0000

    made rubocop happy for repository_observer

--- lib/hold/sequel/repository_observer.rb
+++ lib/hold/sequel/repository_observer.rb
@@ -1,28 +1,32 @@
-# If you want to observe events on a Hold::Sequel::IdentitySetRepository
-# you need to implement this interface. Stubs supplied here to save you some
-# boilerplate in case you only care about certain events.
-#
-# The callback methods correspond to their counterparts on
-# Hold::Sequel::IdentityHashRepository but with an added argument
-# passing the repository instance.
-#
-# TODO: generalise this to SetRepositories in general
-module Hold::Sequel::RepositoryObserver
-  def pre_insert(repo, entity)
-  end
+module Hold
+  module Sequel
+    # If you want to observe events on a Hold::Sequel::IdentitySetRepository
+    # you need to implement this interface. Stubs supplied here to save you some
+    # boilerplate in case you only care about certain events.
+    #
+    # The callback methods correspond to their counterparts on
+    # Hold::Sequel::IdentityHashRepository but with an added argument
+    # passing the repository instance.
+    #
+    # TODO: generalise this to SetRepositories in general
+    module RepositoryObserver
+      def pre_insert(_repo, _entity)
+      end
 
-  def post_insert(repo, entity, insert_rows, insert_id)
-  end
+      def post_insert(_repo, _entity, _insert_rows, _insert_id)
+      end
 
-  def pre_update(repo, entity, update_entity)
-  end
+      def pre_update(_repo, _entity, _update_entity)
+      end
 
-  def post_update(repo, entity, update_entity, update_rows)
-  end
+      def post_update(_repo, _entity, _update_entity, _update_rows)
+      end
 
-  def pre_delete(repo, entity)
-  end
+      def pre_delete(_repo, _entity)
+      end
 
-  def post_delete(repo, entity)
+      def post_delete(_repo, _entity)
+      end
+    end
   end
 end
